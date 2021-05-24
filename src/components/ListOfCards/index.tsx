afb9a5aa366bb5b76af00f76d534c7f4283dcf9e
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-05-24 22:12:21 +0200

    🚀 ListOfCards: a new component

---
+++ src/components/ListOfCards/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Card, { TCard } from '../Card'
+import List from '../List'
+import * as S from './styles'
+
+interface Item extends TCard {
+  slug: string
+}
+
+type TListOfCards = {
+  items: Item[]
+}
+
+const ListOfCards = ({ items }: TListOfCards) => (
+  <List columns={3}>
+    {items.map((item) => (
+      <Link to={item.slug}>
+        <S.ListItem>
+          <Card {...item} layout="auto" />
+        </S.ListItem>
+      </Link>
+    ))}
+  </List>
+)
+
+export default ListOfCards
