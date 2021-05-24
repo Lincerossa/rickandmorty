469cf3308b2c0deb71a6f56ef0a6bd7faed8d975
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-05-24 15:43:34 +0200

    🚀 feat: App.tsx wrapped by ThemeProvider + added the GlobalStyle

---
+++ src/styles/global.ts
@@ -0,0 +1,82 @@
+import { createGlobalStyle } from 'styled-components'
+import theme from './theme'
+
+export default createGlobalStyle`
+
+  html, body {
+    height: auto;
+    font-size: 1rem;
+    color: black;
+    margin: 0;
+  }
+
+  strong {
+    font-weight: 600;
+  }
+  h1, h2, h3, h4 {
+    font-weight: 500;
+    color: ${theme.colors.primary};
+    position: relative;
+    &:before {
+      content:"";
+      width: 5rem;
+      border-bottom: 2px solid ${theme.colors.thirdary};
+      position: absolute;
+      left: 0;
+      bottom: 0;
+    }
+  }
+
+  h1 {
+    font-size: 2rem;
+    @media (min-width: 768px){
+      font-size: 3rem;
+    }
+  }
+
+  h2 {
+    font-size: 1.5rem;
+    @media (min-width: 768px){
+      font-size: 2rem;
+    }
+  }
+
+  h3 {
+    font-size: 1.375rem;
+    @media (min-width: 768px){
+      font-size: 1.5
+    }
+  }
+
+  p, span {
+    font-weight: 400;
+    font-size: 1.25rem;
+    line-height: 1.75;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+    font-family: 'Open Sans', sans-serif;
+
+  }
+
+  a{
+    color: ${theme.colors.primary} !important;
+    text-decoration: none;
+    &:hover{
+      color: ${theme.colors.thirdary} !important;
+    }
+  }
+  a > * {
+    color: ${theme.colors.primary} !important;
+  }
+  
+
+  ::selection {
+    color: rgb(255, 255, 255);
+    background: ${theme.colors.primary};
+  }
+
+`
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+const theme = {
+  colors: {
+    primary: '#e84545',
+    secondary: '#2b2e4a',
+    thirdary: '#f08a5d',
+    light: '#fdfaf6'
+  }
+}
+
+export default theme
