af036ba65b13ce909acd01e149804d9555f9072b
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-05-25 21:56:54 +0200

    🚀 Character: mvp for all the required data fetching

--- src/pages/Character.tsx
+++ src/pages/Character.tsx
@@ -5,34 +5,48 @@ import produceCardProps from '../utility/produceCardProps'
 import { Background } from '../components/Background/styles'
 import theme from '../styles/theme'
 
+function produceListOfText({chapterNames}: {chapterNames: []}){
+  return [
+    '<h1>Bio</h1>',
+    '<h1>Origin</h1>',
+    '<h1>Location</h1>',
+    `<h1>Chapters</h1>
+      <ul>
+        ${chapterNames.map(({name}: {name: string}) => `<li>${name}</li>`)}
+      </ul>
+    `
+  ]
+}
+
 export default ({match: {params: {id}}}: any) => {
   const [data, setData]= useState<any>(null)
-  const cardProps = data && produceCardProps(data)
   const [isLoading, setLoading] = useState<boolean>(false)
+  const cardProps = data && produceCardProps(data.character)
+  const listOfText = data && produceListOfText(data)
 
   useEffect(() => {
     if(!id) return
     async function fetchData(){
       setLoading(true)
-      const result = await axios.get(`https://rickandmortyapi.com/api/character/${id}`)
-        .then(result => result.data)  
-        .catch(() => setData(null))
+      try {
+        const character = await axios.get(`https://rickandmortyapi.com/api/character/${id}`).then(result => result.data)  
+        const location = character?.location?.url && await axios.get(character.location.url).then(result => result.data)  
+        const origin = character?.origin?.url && await axios.get(character.origin.url).then(result => result.data)
+        const episodes = character.episode?.length > 0 && character.episode.map((  e:any) => e.split('/').slice(-1))
+        const chapterNames = episodes && await axios.get(`https://rickandmortyapi.com/api/episode/${episodes}`).then(response => response.data)
+        setData({ character, chapterNames, location, origin })
+      } catch (error) {
+        setData(null)
+      }
 
-      if(result) setData(result)
       setLoading(false)
     }
     fetchData()
   }, [id])
   
-  const listOfText = [
-    '<h1>Bio</h1>',
-    '<h1>Origin</h1>',
-    '<h1>Location</h1>',
-    '<h1>Chapters</h1>'
-  ]
-
   return (
     <Layout isLoading={isLoading}>
+      {!isLoading && !data && <div>NO DATA</div>}
       {data && (
         <>
           <Wrapper size="big">
@@ -40,7 +54,7 @@ export default ({match: {params: {id}}}: any) => {
               <Card {...cardProps} />
             </Padder>
           </Wrapper>
-          {listOfText.map((text, i) => (
+          {listOfText.map((text:string, i:number) => (
             <Background key={text} background={theme.colors[i%2 ? 'secondary' : 'primary']} color="white">
               <Wrapper size="large">
                 <Padder size="large">
@@ -49,7 +63,6 @@ export default ({match: {params: {id}}}: any) => {
               </Wrapper>
             </Background>
           ))}
-          
         </>
       )}
     </Layout>
