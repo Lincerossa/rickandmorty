0b3b901140232d9fa9f4acd8b64e5cb9f7e49ef3
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-05-26 22:26:58 +0200

    🔧 FiltersBar: fixed empty option

--- src/components/FiltersBar/index.tsx
+++ src/components/FiltersBar/index.tsx
@@ -7,14 +7,14 @@ export default ({ onChange, initialState } : TFiltersBarProps) => {
     <S.FiltersBar>
       <input placeholder="Name" type="text" value={initialState.name} onChange={e => onChange('name', e.target.value)} />
       <select value={initialState.gender} onChange={e => onChange('gender', e.target.value)}>
-        <option value={undefined}>Gender</option>
+        <option value={''}>Gender</option>
         <option value="male">Male</option>
         <option value="female">Female</option>
         <option value="genderless">Genderless</option>
         <option value="unknown">Unknown</option>
       </select>
       <select value={initialState.status} onChange={e => onChange('status', e.target.value)}>
-        <option value={undefined}>Status</option>
+        <option value={''}>Status</option>
         <option value="alive">Alive</option>
         <option value="dead">Dead</option>
         <option value="unknown">Unknown</option>
